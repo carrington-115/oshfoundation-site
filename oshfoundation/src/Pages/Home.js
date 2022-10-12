cc0ccea5a12f7537c8153a3722ee17b075ce21ba
Mark Carrington <frumarkcarrington@gmail.com>
2022-10-12 13:43:11 +0100

    added pages and headers

--- oshfoundation/src/Pages/Home.js
+++ oshfoundation/src/Pages/Home.js
@@ -1,7 +1,49 @@
 import React from "react";
-
+import styled from "styled-components";
 function Home() {
-  return <div>Home</div>;
+  return (
+    <Container>
+      <VideoIntro>video</VideoIntro>
+      <Content>
+        <div className="content">Content here!</div>
+        <aside>an aside</aside>
+      </Content>
+    </Container>
+  );
 }
 
 export default Home;
+
+let Container = styled.section`
+  padding: 0 0.5cm;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  display: flex;
+  flex-direction: column;
+`;
+
+let Content = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  height: 100vh;
+  .content,
+  aside {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    font-family: times new roman;
+    border: 2px solid black;
+    heigth: 100vh;
+  }
+  .content{
+    width: 70%;
+  }
+  aside{
+    width: 30%;
+  }
+`;
+
+let VideoIntro = styled.div``;
--- oshfoundation/src/components/Header.js
+++ oshfoundation/src/components/Header.js
@@ -1,8 +1,64 @@
 import React from "react";
-import { HeaderContainer } from "./Styles";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 function Header() {
-  return <HeaderContainer>Header</HeaderContainer>;
+  return (
+    <Container>
+      <PageLink to="/" logo={true}>
+        <div className="logo">
+          <img src="/images/logo.png" alt="oasis-of-hope-logo" />
+        </div>
+      </PageLink>
+      <nav>
+        <PageLink to="/" logo={false}>
+          Home
+        </PageLink>
+        <PageLink to="aboutus" logo={false}>
+          About us
+        </PageLink>
+        <PageLink to="services" logo={false}>
+          Services
+        </PageLink>
+        <PageLink to="blog" logo={false}>
+          Blog
+        </PageLink>
+        <PageLink to="contactus" logo={false}>
+          Contact us
+        </PageLink>
+      </nav>
+    </Container>
+  );
 }
 
 export default Header;
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 10px 1cm;
+  nav {
+    margin-right: 2.5cm;
+  }
+  .logo {
+    width: 15vw;
+    img {
+      width: 100%;
+      height: 100%;
+    }
+  }
+`;
+const PageLink = styled(Link)`
+  text-decoration: none;
+  margin: 0 10px;
+  font-size: 12.5px;
+  padding-bottom: 5px;
+  color: green;
+  border-bottom: 1px solid transparent;
+  &:hover {
+    color: black;
+    border-bottom-color: ${(props) => (props.logo ? "transparent" : "green")};
+    transition: 0.25s 250ms ease-out;
+  }
+`;
